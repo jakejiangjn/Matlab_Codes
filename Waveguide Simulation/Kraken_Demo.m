15052d4c07be74249c7b2d73cf14b2ecf543a881
姜景宁(Jake Jiang) <jakejiangjn@users.noreply.github.com>
2014-05-13 09:59:52 +0800

    Update Kraken_Demo.m

--- Waveguide Simulation/Kraken_Demo.m
+++ Waveguide Simulation/Kraken_Demo.m
@@ -2,52 +2,57 @@ clear all;close all;clc;
 %% Generate field.flp
 titleflp = 'field'; % to generate field.flp
 SD = 10; % Source Elements' Depth in m
-RD = linspace(0.0,200,201); % Each Source Receive Aarriy Elements' Depth in m
-R = linspace(0.0,0.2,201); % Source Receive Aarries' Location in km
-Pos = struct('s',struct('depth',SD),'r',struct('depth',RD,'range',R));
+RD = linspace(0.0,100,101); % Each Source Receive Aarry Elements' Depth in m
+R = linspace(0.0,2,201); % Source Receive Aarries' Location in km
+Pos.theta = [];  Pos.s.depth = SD;
+Pos.r.depth = RD;   Pos.r.range = R;
 write_fieldflp( 'RA', Pos );
 %% Generate env file
-envfil = 'KrakenDemo'; % to generate MunkKraken.env
-freq = 1e3; % frequency
+envfil = 'KrakenDemo'; % to generate KrakenDemo.env
+freq = 8e3; % frequency
 % Boundary Codition Setting
 Bdry.Top.Opt = 'NVW'; % Options of Top Boundary
-Bdry.Top.Hs.alphaR = [];
-Bdry.Top.Hs.betaR = [];
+Bdry.Top.Hs.alphaR = [];    Bdry.Top.Hs.betaR = [];
 Bdry.Top.Hs.rho = [];
-Bdry.Top.Hs.alphaI = [];
-Bdry.Top.Hs.betaI = [];
+Bdry.Top.Hs.alphaI = [];    Bdry.Top.Hs.betaI = [];
 Bdry.Bot.Opt = 'A'; % Options of Bottom Boundary
-Bdry.Bot.HS.alphaR = 2000; % CPT -> P-wave Sound Speed of Bottom Boundary
-Bdry.Bot.HS.betaR = 0; % CST -> S-wave Sound Speed of Bottom Boundary
-Bdry.Bot.HS.rho = 2.0; % RHOT -> density of Bottom Boundary
+Bdry.Bot.HS.alphaR = 2000; % CPT -> P-wave Sound Speed of Bottom Boundary(m/s)
+Bdry.Bot.HS.betaR = 0; % CST -> S-wave Sound Speed of Bottom Boundary(m/s)
+Bdry.Bot.HS.rho = 2.0; % RHOT -> density of Bottom Boundary(g/cm^3)
 Bdry.Bot.HS.alphaI = 0.3; % APT -> Attenuation of P-wave Bottom Boundary
 Bdry.Bot.HS.betaI = 0; % AST -> Attenuation of S-wave Bottom Boundary
 % Layer Media Setting
-SSP.NMedia = 1; % Media Numbers between Top and Rigid Bottom 
-Layer(1).z = linspace(0,200,201);
-L = length( Layer(1).z );
-Layer(1).alphaR = linspace( 1460,1450, L );
+SSP.NMedia = 2; % Media Numbers between Top and Rigid Bottom
+Layer(1).z = linspace(0,100,101);   L = length( Layer(1).z );
+Layer(1).alphaR = linspace( 1500,1500, L );
 Layer(1).betaR = zeros( 1, L );
 Layer(1).rho = ones( 1, L );
 Layer(1).alphaI = zeros( 1, L );
 Layer(1).betaI = zeros( 1, L );
-SSP.N = 25000; % NMesh of each Layer -> integer array
-SSP.sigma = zeros(1,1); % RMS roughness at the interface
-SSP.depth = [0 200]; % Starting Depth of each Media Layer and the last element is the depth of Bottom
+Layer(2).z = linspace(100,120,21);   L = length( Layer(2).z );
+Layer(2).alphaR = linspace( 1600,1600, L );
+Layer(2).betaR = zeros( 1, L );
+Layer(2).rho = ones( 1, L )*1.9;
+Layer(2).alphaI = ones( 1, L )*0.5;
+Layer(2).betaI = zeros( 1, L );
+SSP.N = [8000 2000]; % NMesh of each Layer -> integer array
+% NMesh(n) = ( >10 ) * Layer(n).z(end) * max(freq) / min(Layer(n).alphaR)
+SSP.sigma = zeros(1,2); % RMS roughness at the interface
+SSP.depth = [0 100 120]; % Starting Depth of each Media Layer and the last element is the depth of Bottom
 SSP.raw = Layer;
 Beam = struct();
-cInt = struct('Low',1400,'High',1600);
-RMax = 0;
+cInt.Low = 1400; cInt.High = 1600;  RMax = 0;
 clear Layer L;
 if exist([envfil '.env'], 'file')
     delete([envfil '.env']);
 end;
 write_env( envfil, 'KRAKEN', envfil, freq, SSP, Bdry, Pos, Beam, cInt, RMax, [] );
-%write_env( envfil, model, TitleEnv, freq, SSP, Bdry, Pos, Beam, cInt, RMax, varargin );
+% syntax: write_env( envfil, model, TitleEnv, freq, SSP, Bdry, Pos, Beam, cInt, RMax, varargin );
 %% Run Kraken
+% generate KrakenDemo.shd and KrakenDemo.mod
 kraken( envfil ); % krakenc( envfil );
 %% Post processing
-[PlotTitle,PlotType,~,alten,~,p] = read_shd( [envfil '.shd'] );
+[~,~,~,atten,~,p] = read_shd( [envfil '.shd'] );
 % Struct 'Pos' includes 3 structs 'Pos.theta', 'Pos.s'('Pos.s.depth' array of NSD*1) and
 % 'Pos.r'('Pos.r.depth' array of NRD*1;'Pos.r.range' array of NR*1)
 % size(p) = length(SD)*length(RD)*length(R)
@@ -57,7 +62,7 @@ figure(1); pcolor(R,RD,Green); % pcolor(Pos.r.range,Pos.r.depth,Green);
 xlabel('range/km');  ylabel('depth/m');
 shading interp; % interpolation for smooth display
 axis ij; % Set the Origin Point to the up-left corner
-caxis([-30 -0]); % Set the display range
+caxis([-30 0]); % Set the display range
 colorbar;
 %%
 figure(2);
